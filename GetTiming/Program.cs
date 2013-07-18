3ddda9213f5cd9b3ac376d94a5091a425304a6bd
Giancarlo Todone <giancarlotodone@yahoo.it>
2013-07-18 17:35:33 +0200

    median; formatting; options

--- GetTiming/Program.cs
+++ GetTiming/Program.cs
@@ -8,43 +8,125 @@ namespace GetTiming
 {
     class Program
     {
+        public enum ArgsParserState
+        { 
+            ExpectingParam,
+            ParseNExecutions,
+            ParseExeAndParams
+        }
+
         static void Main(string[] args)
         {
-            const int times = 10;
-
             long frequency = Stopwatch.Frequency;
             double nanosecPerTick = (double)1000000000 / (double)frequency;
 
             if (args.Length < 1)
             {
-                Console.WriteLine("usage: getTiming [exe filename] [exe params]");
-                Console.WriteLine();
-                // Display the timer frequency and resolution. 
-                if (Stopwatch.IsHighResolution)
-                {
-                    Console.WriteLine("Operations timed using the system's high-resolution performance counter.");
-                }
-                else
-                {
-                    Console.WriteLine("Operations timed using the DateTime class.");
-                }
-                Console.WriteLine("  Timer frequency in ticks per second = {0}", frequency);
-                Console.WriteLine("  Timer is accurate within {0:0} nanoseconds", nanosecPerTick);
+                Console.WriteLine(@"
+usage: getTiming [options] [-e] [exe filename] [exe params]
+
+options:
+ -i  prints timer info
+ -h  prints header
+ -w  forces execution with no console
+ -n  sets number of executions
+ -e  must be last param; specifies excutable and params to execute
+");
                 return;
             }
 
-            List<string> parametersList = new List<string>(args);
-            parametersList.RemoveAt(0);
+            var parseState = ArgsParserState.ExpectingParam;
+            bool nowin = false;
+            int nExec = 10;
+            string exeName = null;
+
+            List<string> exeParams = null;
+
+            int paramsPos = 0;
+
+            bool done = false;
+
+            foreach(string s in args)
+            {
+                switch (parseState)
+                {
+                    case ArgsParserState.ExpectingParam:
+                        switch (s)
+                        {
+                            case "-i":
+                                // Display the timer frequency and resolution. 
+                                if (Stopwatch.IsHighResolution)
+                                {
+                                    Console.WriteLine("Operations timed using the system's high-resolution performance counter.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Operations timed using the DateTime class.");
+                                }
+                                Console.WriteLine("  Timer frequency in ticks per second = {0}", frequency);
+                                Console.WriteLine("  Timer is accurate within {0:0} nanoseconds", nanosecPerTick);
+                                return;
+
+                            case "-h":
+                                Console.WriteLine("{0,11:##########}|{1,11:###########}|{2,11:###########}|{3,11:###########}|{4,11:###########}|{5,11:###########}", "exe name", "minimum", "average", "median", "maximum", "proc.");
+                                return;
+
+                            case "-n":
+                                parseState = ArgsParserState.ParseNExecutions;
+                                break;
+
+                            case "-w":
+                                nowin = true;
+                                break;
+
+                            case "-e":
+                                parseState = ArgsParserState.ParseExeAndParams;
+                                break;
+                        }
+                        break;
+
+                    case ArgsParserState.ParseNExecutions:
+                        if (!int.TryParse(s, out nExec))
+                        {
+                            Console.WriteLine("wrong -n param (must be an integer)");
+                            return;
+                        }
+                        if (nExec<3)
+                        {
+                            Console.WriteLine("wrong -n param (must be >= 3)");
+                            return;
+                        }
+                        parseState = ArgsParserState.ExpectingParam;
+                        break;
+
+                    case ArgsParserState.ParseExeAndParams:
+                        exeName = s;
+                        exeParams = (new List<string>(args)).GetRange(paramsPos, args.Length - paramsPos);
+                        done = true; // we won't return to any other state (anything following is parameters to measured exe)
+                        break;
+                }
+
+                if (done) break;
+
+                ++paramsPos;
+            }
+
+            if (string.IsNullOrEmpty(exeName))
+            {
+                Console.WriteLine("no exe name specified!");
+                return;
+            }
+
             string paramsString = "";
-            foreach (string s in parametersList)
+            foreach (string s in exeParams)
             {
                 paramsString += s + " ";
             }
 
-            Console.WriteLine("Executing '{0}' with parameters '{1}'", args[0], paramsString);
+            List<long> times = new List<long>(nExec);
 
-            ProcessStartInfo psi = new ProcessStartInfo(args[0]);
-            psi.Arguments = paramsString;
+            ProcessStartInfo psi = new ProcessStartInfo(exeName, paramsString);
+            psi.CreateNoWindow = nowin;
 
             Stopwatch sw = new Stopwatch();
 
@@ -59,7 +141,7 @@ namespace GetTiming
 
             try
             {
-                for (int i = 0; i < times; ++i)
+                for (int i = 0; i < nExec; ++i)
                 {
                     if (File.Exists("clean.cmd"))
                     {
@@ -71,7 +153,9 @@ namespace GetTiming
                     var proc = Process.Start(psi);
                     proc.WaitForExit();
                     sw.Stop();
-                    
+
+                    times.Add(sw.ElapsedTicks);
+
                     totalProcTime += proc.TotalProcessorTime;
                     totalExecTime += sw.ElapsedTicks;
 
@@ -90,15 +174,28 @@ namespace GetTiming
             }
             catch(Exception e)
             {
-                Console.WriteLine("Exception: {0}", e);
+                Console.WriteLine("Execution failed.");
                 return;
             }
 
-            var avgExecTime = (((double)(totalExecTime - minTime) - maxTime) * (double)nanosecPerTick) / (double)(times - 2);
 
-            var avgProcTime = totalProcTime.TotalMilliseconds / (double)times;
+            long median = 0; 
+            times.Sort();
+            if (nExec % 2 == 0)
+            {
+                median = (times[nExec / 2] + times[(nExec / 2) + 1]) / 2;
+            }
+            else
+            {
+                median = times[nExec / 2];
+            }
+            
 
-            Console.WriteLine("v: {0,11:###########} / -: {1,11:###########} / ^: {2,11:###########} / p: {3,11:###########} \n\n", minTime * (double)nanosecPerTick, avgExecTime, maxTime * (double)nanosecPerTick, avgProcTime * 1000);
+            var avgExecTime = (((double)(totalExecTime - minTime) - maxTime) * (double)nanosecPerTick) / (double)(nExec - 2);
+
+            var avgProcTime = totalProcTime.TotalMilliseconds / (double)nExec;
+
+            Console.WriteLine("{0,11:##########}|{1,11:###########}|{2,11:###########}|{3,11:###########}|{4,11:###########}|{5,11:###########}", exeName, minTime * nanosecPerTick, avgExecTime, median * nanosecPerTick, maxTime * nanosecPerTick, avgProcTime * 1000);
             
         }
     }
